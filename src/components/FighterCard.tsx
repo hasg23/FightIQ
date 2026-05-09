67f8d3f69c29ef67332692f412a7b39d9debd242
Heber <hebersolisg@gmail.com>
2026-05-09 08:34:42 -0600

    feat: added style, fighterCard, shows cleaner design, and info

---
+++ src/components/FighterCard.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import type { Fighter } from "../types/Fighter";
+
+interface FighterCardProps {
+    fighter: Fighter
+}
+function FighterCard({ fighter }: FighterCardProps) {
+    return (
+        <div>
+            <Card sx={{ maxWidth: 345, alignItems: "center", display: "flex", flexDirection: "column", justifyContent: "center" }}>
+                <CardContent>
+                    <Typography variant="h5" component="div" sx={{ fontSize: 14, alignItems: "center", display: "flex", flexDirection: "column" }}>
+                        {fighter.name}
+                    </Typography>
+
+                </CardContent>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    width="140"
+                    image={fighter.photo}
+                    onError={(e) => e.currentTarget.src = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'}
+                    alt={fighter.name}
+                    sx={{ borderRadius: 4, width: 140 }}
+                />
+                <Typography variant="h2" component="div" sx={{ fontSize: 14, alignItems: "center", display: "flex", flexDirection: "column" }}>
+                    {fighter.nickname ?? "No nickname"}
+                </Typography>
+            </Card>
+
+        </div>
+    )
+
+}
+export default FighterCard
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import type { Fighter } from "../types/Fighter"
 import { searchFighters } from "../services/fighterService"
-import { Box, CircularProgress } from "@mui/material"
+import { Box, CircularProgress, Grid } from "@mui/material"
+import FighterCard from "../components/FighterCard"
 
 
 function Home() {
@@ -45,18 +46,13 @@ function Home() {
             {
                 loading ? <Box sx={{ display: "flex", justifyContent: "center", marginTop: "20px" }}><CircularProgress /></Box> :
                     error ? <p>{error}</p> :
-                        fighter.map((f) => (
-                            <div key={f.id}>
-                                <p>{f.name}</p>
-                                <p>{f.nickname}</p>
-                                <img src={f.photo} alt={f.name} />
-                                <p>{f.height}</p>
-                                <p>{f.weight}</p>
-                                <p>{f.reach}</p>
-                                <p>{f.stance}</p>
-                                <p>{f.category}</p>
-                            </div>
-                        ))
+                        <Grid container spacing={2}>
+                            {fighter.map((f) => (
+                                <Grid sx={{ xs: 12, sm: 6, md: 3 }} key={f.id}>
+                                    <FighterCard fighter={f} />
+                                </Grid>
+                            ))}
+                        </Grid>
             }
         </div>
     )
